19b9a3832cebfe0f5685acf78096a339efbdb71c
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 18:16:38 +0100

    add HolidaySearch class with only constructor, add null input check in test, modify constructor to pass

---
+++ HolidaySearchLibrary.Tests/HolidaySearchTests.cs
@@ -0,0 +1,45 @@
+﻿namespace HolidaySearchLibrary.Tests;
+internal class HolidaySearchTests
+{
+    private HolidaySearch _holidaySearch;
+
+    [Test]
+    public void Constructor_With_Null_DepartingFrom_Should_Throw_ArgumentNullException()
+    {
+        // Arrange
+        string departingFrom = null;
+        string travelingTo = "AGP";
+        DateTime departureDate = DateTime.Parse("2023/07/01");
+        int duration = 7;
+
+        // Act
+        Action act = () => _holidaySearch = new HolidaySearch(
+            departingFrom,
+            travelingTo,
+            departureDate,
+            duration);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Constructor_With_Null_TravelingTo_Should_Throw_ArgumentNullException()
+    {
+        // Arrange
+        string departingFrom = "MAN";
+        string travelingTo = null;
+        DateTime departureDate = DateTime.Parse("2023/07/01");
+        int duration = 7;
+
+        // Act
+        Action act = () => _holidaySearch = new HolidaySearch(
+            departingFrom,
+            travelingTo,
+            departureDate,
+            duration);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
---
+++ HolidaySearchLibrary/HolidaySearch.cs
@@ -0,0 +1,12 @@
+﻿namespace HolidaySearchLibrary;
+public class HolidaySearch
+{
+    public HolidaySearch(string departingFrom, string travelingTo, DateTime departureDate, int duration)
+    {
+        if (departingFrom is null)
+            throw new ArgumentNullException(nameof(departingFrom));
+
+        if (travelingTo is null)
+            throw new ArgumentNullException(nameof(travelingTo));
+    }
+}
