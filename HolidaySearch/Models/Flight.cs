ba70bc8fcc96ee12941e85ee5880322d1e70f438
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 09:56:50 +0100

    add flight and hotel classes

---
+++ HolidaySearch/Models/Flight.cs
@@ -0,0 +1,10 @@
+﻿namespace HolidaySearch.Models;
+public class Flight
+{
+    public int Id { get; set; }
+    public string Airline { get; set; }
+    public string From { get; set; }
+    public string To { get; set; }
+    public int Price { get; set; }
+    public DateTime DepartureDate { get; set; }
+}
