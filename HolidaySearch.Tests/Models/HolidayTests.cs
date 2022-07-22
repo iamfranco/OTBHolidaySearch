c8af236b42055c490ad0fe474ca93ac97977a3a6
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 15:15:15 +0100

    add Holiday class with unit test on constructor null inputs

---
+++ HolidaySearch.Tests/Models/HolidayTests.cs
@@ -0,0 +1,61 @@
+﻿using HolidaySearch.Models;
+
+namespace HolidaySearch.Tests.Models;
+internal class HolidayTests
+{
+    [Test]
+    public void Constructor_With_Null_Flight_Input_Should_Throw_ArgumentNullException()
+    {
+        // Arrange
+        Flight flight = null;
+        Hotel hotel = GetHotel();
+
+        // Act
+        Holiday holiday;
+        Action act = () => holiday = new Holiday(flight, hotel);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Constructor_With_Null_Hotel_Input_Should_Throw_ArgumentNullException()
+    {
+        // Arrange
+        Flight flight = GetFlight();
+        Hotel hotel = null;
+
+        // Act
+        Holiday holiday;
+        Action act = () => holiday = new Holiday(flight, hotel);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    private Flight GetFlight()
+    {
+        return new()
+        {
+            Id = 1,
+            Airline = "First Class Air",
+            From = "MAN",
+            To = "TFS",
+            Price = 470,
+            DepartureDate = DateTime.Parse("2023-07-01")
+        };
+    }
+
+    private Hotel GetHotel()
+    {
+        return new()
+        {
+            Id = 1,
+            Name = "Iberostar Grand Portals Nous",
+            ArrivalDate = DateTime.Parse("2022-11-05"),
+            PricePerNight = 100,
+            LocalAirports = new List<string>() { "TFS" },
+            Nights = 7
+        };
+    }
+}
---
+++ HolidaySearch/Models/Holiday.cs
@@ -0,0 +1,18 @@
+﻿namespace HolidaySearch.Models;
+public class Holiday
+{
+    private Flight _flight;
+    private Hotel _hotel;
+
+    public Holiday(Flight flight, Hotel hotel)
+    {
+        if (flight is null)
+            throw new ArgumentNullException(nameof(flight));
+
+        if (hotel is null)
+            throw new ArgumentNullException(nameof(hotel));
+
+        _flight = flight;
+        _hotel = hotel;
+    }
+}
