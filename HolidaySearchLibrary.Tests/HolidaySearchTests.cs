45ccb3c10d292a7d3eb0b92c144d3a9cc9706854
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 18:30:32 +0100

    add test case for Customer 1's input, modify HolidaySearch to pass

--- HolidaySearchLibrary.Tests/HolidaySearchTests.cs
+++ HolidaySearchLibrary.Tests/HolidaySearchTests.cs
@@ -1,4 +1,6 @@
-﻿namespace HolidaySearchLibrary.Tests;
+﻿using HolidaySearchLibrary.Models;
+
+namespace HolidaySearchLibrary.Tests;
 internal class HolidaySearchTests
 {
     private HolidaySearch _holidaySearch;
@@ -41,5 +43,47 @@ internal class HolidaySearchTests
 
         // Assert
         act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Constructor_With_Customer_1_Input_Then_Results_First_Should_Return_Correct_Holiday()
+    {
+        // Arrange
+        string departingFrom = "MAN";
+        string travelingTo = "AGP";
+        DateTime departureDate = DateTime.Parse("2023/07/01");
+        int duration = 7;
+
+        Holiday expectedFirstResult = new(
+            flight : new()
+            {
+                Id = 2,
+                Airline = "Oceanic Airlines",
+                From = "MAN",
+                To = "AGP",
+                Price = 245,
+                DepartureDate = DateTime.Parse("2023-07-01")
+            },
+            hotel : new()
+            {
+                Id = 9,
+                Name = "Nh Malaga",
+                ArrivalDate = DateTime.Parse("2023-07-01"),
+                PricePerNight = 83,
+                LocalAirports = new List<string>() { "AGP" },
+                Nights = 7
+            });
+
+        // Act
+        _holidaySearch = new HolidaySearch(
+            departingFrom,
+            travelingTo,
+            departureDate,
+            duration);
+
+        List<Holiday> results = _holidaySearch.Results;
+
+        // Assert
+        results.First().Should().BeEquivalentTo(expectedFirstResult);
     }
 }
--- HolidaySearchLibrary/HolidaySearch.cs
+++ HolidaySearchLibrary/HolidaySearch.cs
@@ -1,4 +1,7 @@
-﻿namespace HolidaySearchLibrary;
+﻿using HolidaySearchLibrary.Models;
+using HolidaySearchLibrary.Services;
+
+namespace HolidaySearchLibrary;
 public class HolidaySearch
 {
     public HolidaySearch(string departingFrom, string travelingTo, DateTime departureDate, int duration)
@@ -8,5 +11,21 @@ public class HolidaySearch
 
         if (travelingTo is null)
             throw new ArgumentNullException(nameof(travelingTo));
+
+        string currentDirectory = Directory.GetCurrentDirectory();
+        string flightFilePath = currentDirectory + @"\dataFiles\flightData.json";
+        string hotelFilePath = currentDirectory + @"\dataFiles\hotelData.json";
+
+        IReaderService<Flight> flightReaderService = new ReaderService<Flight>(flightFilePath);
+        IReaderService<Hotel> hotelReaderService = new ReaderService<Hotel>(hotelFilePath);
+
+        IFlightSearchService flightSearchService = new FlightSearchService(flightReaderService);
+        IHotelSearchService hotelSearchService = new HotelSearchService(hotelReaderService);
+
+        HolidaySearchService holidaySearchService = new HolidaySearchService(flightSearchService, hotelSearchService);
+
+        Results = holidaySearchService.Search(departingFrom, travelingTo, departureDate, duration);
     }
+
+    public List<Holiday> Results { get; }
 }
