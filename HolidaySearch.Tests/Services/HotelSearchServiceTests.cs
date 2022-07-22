8c8325745a4dcb0e54dd3c1dc3b991e09e8aa267
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 13:00:31 +0100

    add HotelSearchService class with only constructor, with unit test

---
+++ HolidaySearch.Tests/Services/HotelSearchServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using HolidaySearch.Models;
+using HolidaySearch.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Tests.Services;
+internal class HotelSearchServiceTests
+{
+    Mock<IReaderService<Hotel>> _hotelReaderServiceMock;
+    HotelSearchService _hotelSearchService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _hotelReaderServiceMock = new Mock<IReaderService<Hotel>>();
+        _hotelSearchService = new HotelSearchService(_hotelReaderServiceMock.Object);
+    }
+}
---
+++ HolidaySearch/Services/HotelSearchService.cs
@@ -0,0 +1,12 @@
+﻿using HolidaySearch.Models;
+
+namespace HolidaySearch.Services;
+public class HotelSearchService
+{
+    private IReaderService<Hotel> _hotelReaderService;
+
+    public HotelSearchService(IReaderService<Hotel> hotelReaderService)
+    {
+        _hotelReaderService = hotelReaderService;
+    }
+}
