8ee49cdfbe176f4941bf7a593cd0fc8f21618475
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 10:40:34 +0100

    add FlightReaderService with unit test, only test for null input in constructor

---
+++ HolidaySearch.Tests/Services/FlightReaderServiceTests.cs
@@ -0,0 +1,20 @@
+﻿using HolidaySearch.Services;
+
+namespace HolidaySearch.Tests.Services;
+internal class FlightReaderServiceTests
+{
+    private FlightReaderService _flightReaderService;
+
+    [Test]
+    public void Constructor_With_Null_Input_Should_Throw_ArgumentNullException()
+    {
+        // Arrange
+        string filePath = null;
+
+        // Act
+        Action act = () => _flightReaderService = new FlightReaderService(filePath);
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+}
---
+++ HolidaySearch/Services/FlightReaderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Services;
+public class FlightReaderService
+{
+    private string _filePath;
+
+    public FlightReaderService(string filePath)
+    {
+        if (filePath is null)
+            throw new ArgumentNullException(nameof(filePath));
+
+        _filePath = filePath;
+    }
+}
