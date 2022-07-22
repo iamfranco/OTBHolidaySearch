613363194e569df3f11a80e6fb38d1ae22342952
iamfranco <kinfaifrancochan@gmail.com>
2022-07-22 11:00:10 +0100

    add unit test for FlightReaderService.Read to read from filePath, modify FlightReaderService and Flight to pass and add test json files into unit test project

--- HolidaySearch/Services/FlightReaderService.cs
+++ HolidaySearch/Services/FlightReaderService.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using HolidaySearch.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HolidaySearch.Services;
@@ -15,5 +17,14 @@ public class FlightReaderService
             throw new ArgumentNullException(nameof(filePath));
 
         _filePath = filePath;
+    }
+
+    public List<Flight> Read()
+    {
+        string jsonString = File.ReadAllText(_filePath);
+
+        List<Flight> flights = JsonSerializer.Deserialize<List<Flight>>(jsonString);
+
+        return flights;
     }
 }
